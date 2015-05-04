e3b2d7ec17cd11d9669735dbab4bbd04806cb152
darkryder <sambhav13085@iiitd.ac.in>
2015-05-04 23:30:41 +0530

    Completed the implementation of autocomplete suggestions. Popovers are shown with suggestions.

--- static/js/autocomplete.js
+++ static/js/autocomplete.js
@@ -1,54 +1,49 @@
 // author: darkryder - https://github.com/darkryder/
 
-// set type variable in window js variable
-// #searching is a png for searching
-// #responsebox is the box with the answers
-
 $(document).ready(function(){
 	var search_results = {};
-	var responsebox = $("#responsebox");
 	var responses = $("#responses")[0];
 
 	var category = window.item_category_to_autosearch;
 
-	responsebox.fadeOut(0)
-
-
-
 	function show_response(data){
-		$("#searching").hide();
 		if (data.length == 0) {
-			responsebox.fadeOut(100)
+			$("#item_name_field").popover('destroy');
 		}
 		else {
-			responses.innerHTML = "";
+			var a = "<b>Did you mean?</b><br /><ul>"
 			for (var i = 0; i < data.length; i++) {
-				responses.innerHTML += '<a href="#" style="color:black" \
+				a += '<li><a href="#" style="color:black" \
 										class="response" data-toggle="modal"\
 										data-remote="true" data-url="'
 										+ data[i]['url'] + '"><b>  '
-										+ data[i]['itemname'] + '</b></a><br />'
+										+ data[i]['itemname'] + '</b></a>'
 			};
+			a += '</ul>';
+			$("#item_name_field").popover('destroy');
+			$("#item_name_field").popover({
+				content: a,
+				focus: false	,
+				html: true
+			}).popover("show");
+
 			// attach the listener again
 			$(".response").click(function(){
 				$("#base-confirmation-modal").modal('hide');
 				$("#base-confirmation-modal").modal('show');
 				$("#base-confirmation-modal").load($(this).data().url);
 			});
-			responsebox.fadeIn(500);
 		}
-
 	}
 	var timer;
 	$("#item_name_field").on('input paste', function(){
 		var value = this.value;
-		if (value === ""){
-			responsebox.fadeOut(100);
+		if (value.trim() == ""){
+			$("#item_name_field").popover('destroy');
 			return;
 		}
 		if (search_results[value] === undefined)
 		{
-			$("#searching").show();
 			clearTimeout(timer);
 			var ms = 300;
 			timer = setTimeout(function(){
